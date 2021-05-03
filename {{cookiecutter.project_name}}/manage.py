87976d6ceeb26204d01b92989af5a6dc06ac90e3
Abhijeet Sonawane <abhijeet@pureid.io>
2021-05-03 20:41:04 +0530

    Removed npm, postcss, tailwindcss, flask-assets

--- {{cookiecutter.project_name}}/manage.py
+++ {{cookiecutter.project_name}}/manage.py
@@ -1,7 +1,6 @@
 import os
 
 from flask_migrate import MigrateCommand
-from flask_assets import ManageAssets
 from flask_script import Manager
 
 from {{cookiecutter.project_slug}} import create_app
@@ -9,7 +8,6 @@ from {{cookiecutter.project_slug}} import create_app
 app = create_app(config=os.environ.get("APP_CONFIG"))
 manager = Manager(app)
 manager.add_command("db", MigrateCommand)
-manager.add_command("assets", ManageAssets())
 
 if __name__ == "__main__":
     manager.run()
--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask_assets import Environment, Bundle
 
 # Flask-SQLAlchemy
 db = SQLAlchemy()
 # Flask-Migrate
 migrate = Migrate()
 
-# Flask-Assets
-assets = Environment()
 
 
 def create_app(config="myapp.config.ProductionConfig"):
@@ -28,14 +25,6 @@ def create_app(config="myapp.config.ProductionConfig"):
     # Initialize add-ons
     db.init_app(app)  # Database
     migrate.init_app(app, db)  # Flask-Migrate
-    assets.init_app(app)  # Flask-Assets
-
-    # Register Assets
-    css = Bundle("src/css/tailwind.css", filters="postcss", output="dist/css/app.css")
-    assets.register("css", css)
-
-    js = Bundle("src/js/*.js", output="dist/js/app.js")
-    assets.register("js", js)
 
     # Include All models
     from {{cookiecutter.project_slug}}.models import User
